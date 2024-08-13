4b298408a71e1ab8446da6d4391e01fecbd4046b
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-08-13 19:30:37 +0300

    Added unit test project and set up build dependencies.

---
+++ QuartoTests/QuartoTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace QuartoTests
+{
+	TEST_CLASS(QuartoTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
