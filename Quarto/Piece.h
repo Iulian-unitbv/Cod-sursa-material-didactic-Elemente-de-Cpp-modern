9a42ae3def776271fdf3a0f7664858fe1d356c19
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-08-16 00:27:55 +0300

    Added enum class for the four features.

--- Quarto/Piece.h
+++ Quarto/Piece.h
@@ -1,4 +1,28 @@
 #pragma once
 class Piece
 {
+public:
+	enum class Body
+	{
+		Full,
+		Hollow
+	};
+
+	enum class Color
+	{
+		Dark,
+		Light
+	};
+
+	enum class Height
+	{
+		Short,
+		Tall
+	};
+
+	enum class Shape
+	{
+		Round,
+		Square
+	};
 };
