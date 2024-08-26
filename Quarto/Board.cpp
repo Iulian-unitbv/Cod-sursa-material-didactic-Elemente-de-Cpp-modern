71547cd1ef22474acd4d62ebc8617f4b776391ce
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-08-26 21:20:36 +0300

    Added basic definition to functions declared in the header.

--- Quarto/Board.cpp
+++ Quarto/Board.cpp
@@ -1,1 +1,17 @@
 #include "Board.h"
+
+const std::optional<Piece>& Board::operator [] (const Position& position) const
+{
+	return std::nullopt;
+}
+
+std::optional<Piece>& Board::operator [] (const Position& position)
+{
+	static std::optional<Piece> dummy;
+	return dummy;
+}
+
+std::ostream& operator << (std::ostream& os, const Board& board)
+{
+	return os;
+}
