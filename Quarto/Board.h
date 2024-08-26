3da19055dc14f2435fe64f361c3e555c6cdf23ca
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-08-26 16:12:46 +0300

    Added matrix of optional Pieces to Board and updated project to use C++17.

--- Quarto/Board.h
+++ Quarto/Board.h
@@ -1,4 +1,15 @@
 #pragma once
+#include <array>
+#include <optional>
+
+#include "Piece.h"
+
 class Board
 {
+private:
+	static constexpr size_t kWidth = 4;
+	static constexpr size_t kHeight = 4;
+
+private:
+	std::array<std::array<std::optional<Piece>, kWidth>, kHeight> m_pieces;
 };
