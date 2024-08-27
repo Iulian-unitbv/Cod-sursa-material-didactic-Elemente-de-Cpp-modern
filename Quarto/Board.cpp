598a8b8818adb89b87f9b723d9b1c684c753383e
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-08-27 18:56:03 +0300

    Added real implementation for [] operators in Board.

--- Quarto/Board.cpp
+++ Quarto/Board.cpp
@@ -2,13 +2,16 @@
 
 const std::optional<Piece>& Board::operator [] (const Position& position) const
 {
-	return std::nullopt;
+	const auto& [row, column] = position;
+	if (row < 0 || row >= kHeight || column < 0 || column >= kWidth)
+		throw std::out_of_range{ "Board position out of bounds." };
+
+	return m_pieces[row][column];
 }
 
 std::optional<Piece>& Board::operator [] (const Position& position)
 {
-	static std::optional<Piece> dummy;
-	return dummy;
+	return const_cast<std::optional<Piece>&>(std::as_const(*this)[position]);
 }
 
 std::ostream& operator << (std::ostream& os, const Board& board)
