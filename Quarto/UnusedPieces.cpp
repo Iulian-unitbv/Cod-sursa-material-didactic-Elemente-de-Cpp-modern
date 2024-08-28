cb0ab4e461203b1748c5efed14b3d243aeaebb00
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-08-28 17:25:28 +0300

    Added constructor for UnusedPieces.

--- Quarto/UnusedPieces.cpp
+++ Quarto/UnusedPieces.cpp
@@ -1,7 +1,19 @@
 #include "UnusedPieces.h"
 
+#include <sstream>
+
 UnusedPieces::UnusedPieces()
 {
+	for (Piece::Body body : {Piece::Body::Full, Piece::Body::Hollow})
+		for (Piece::Color color : {Piece::Color::Dark, Piece::Color::Light})
+			for (Piece::Height height : {Piece::Height::Short, Piece::Height::Tall})
+				for (Piece::Shape shape : {Piece::Shape::Round, Piece::Shape::Square})
+				{
+					Piece piece{ body, color, height, shape };
+					std::ostringstream stream;
+					stream << piece;
+					m_pool.emplace(stream.str(), std::move(piece));
+				}
 }
 
 Piece UnusedPieces::PickPiece(const std::string& name)
