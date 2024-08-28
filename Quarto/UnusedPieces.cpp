b21576d6ffa351d46215d434c851777f60a61548
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-08-28 17:00:20 +0300

    Added dummy implementation for UnusedPieces.

--- Quarto/UnusedPieces.cpp
+++ Quarto/UnusedPieces.cpp
@@ -1,1 +1,15 @@
 #include "UnusedPieces.h"
+
+UnusedPieces::UnusedPieces()
+{
+}
+
+Piece UnusedPieces::PickPiece(const std::string& name)
+{
+	return Piece{ Piece::Body::Full, Piece::Color::Dark, Piece::Height::Short, Piece::Shape::Round };
+}
+
+std::ostream& operator<<(std::ostream& os, const UnusedPieces& unusedPieces)
+{
+	return os;
+}
