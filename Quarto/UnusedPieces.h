4e58d716866a1ac37a52cd2b17af7a583573d648
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-08-28 15:40:02 +0300

    Added data member to UnusedPieces.

--- Quarto/UnusedPieces.h
+++ Quarto/UnusedPieces.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include "Piece.h"
+
+#include <unordered_map>
+
 // Holds all the unused pieces throughout a game.
 class UnusedPieces
 {
+private:
+	std::unordered_map<std::string, Piece> m_pool;
 };
