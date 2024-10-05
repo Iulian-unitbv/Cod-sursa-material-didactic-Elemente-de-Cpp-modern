04b66bba647da149b86a8847ba0fd7922041024f
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-10-06 00:54:05 +0300

    Added dummy implementation for methods in Player.

--- Quarto/Player.cpp
+++ Quarto/Player.cpp
@@ -1,1 +1,20 @@
 #include "Player.h"
+
+Player::Player(std::string&& name)
+{
+}
+
+Piece Player::PickPiece(std::istream& is, UnusedPieces& unusedPieces)
+{
+	return Piece{ Piece::Body::Full, Piece::Color::Dark, Piece::Height::Short, Piece::Shape::Square };
+}
+
+Board::Position Player::PlacePiece(std::istream& is, Piece&& piece, Board& board)
+{
+	return Board::Position{};
+}
+
+std::ostream& operator<<(std::ostream& os, const Player& player)
+{
+	return os;
+}
