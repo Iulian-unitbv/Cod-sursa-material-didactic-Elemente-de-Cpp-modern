94a61ad24915bcefb6724e0d558bb1896ca9b157
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-10-06 01:57:38 +0300

    Added implementation for PlacePiece in Player.

--- Quarto/Player.cpp
+++ Quarto/Player.cpp
@@ -16,7 +16,24 @@ Piece Player::PickPiece(std::istream& is, UnusedPieces& unusedPieces)
 
 Board::Position Player::PlacePiece(std::istream& is, Piece&& piece, Board& board)
 {
-	return Board::Position{};
+	// this little trick is needed here to read numbers instead of ASCII codes
+	uint16_t row;
+	uint16_t column;
+
+	if (is >> row >> column)
+	{
+		Board::Position position{ static_cast<uint8_t>(row), static_cast<uint8_t>(column) };
+		std::optional<Piece>& pieceOpt{ board[position] };
+
+		if (pieceOpt)
+			throw std::logic_error{ "The position is occupied by another piece." };
+
+		pieceOpt = std::move(piece);
+
+		return position;
+	}
+
+	throw std::invalid_argument{ "Only numbers are accepted." };
 }
 
 std::ostream& operator<<(std::ostream& os, const Player& player)
