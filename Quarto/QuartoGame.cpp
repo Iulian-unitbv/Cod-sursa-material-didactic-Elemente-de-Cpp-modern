df880de84c1db86a83aedd1df77ff965c07bc549
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-10-07 23:55:47 +0300

    Initialize all game objects in QuartoGame.

--- Quarto/QuartoGame.cpp
+++ Quarto/QuartoGame.cpp
@@ -1,8 +1,22 @@
 #include "QuartoGame.h"
+#include "Player.h"
+
+#include <iostream>
 
 void QuartoGame::Run()
 {
 	// initialization
+    Board board;
+    UnusedPieces unusedPieces;
+
+    std::string playerName;
+    std::cout << "First player name: ";
+    std::cin >> playerName;
+	Player firstPlayer{ std::move(playerName) };
+
+	std::cout << "Second player name: ";
+	std::cin >> playerName;
+	Player secondPlayer{ std::move(playerName) };
 
 	// update
 	while (true)
