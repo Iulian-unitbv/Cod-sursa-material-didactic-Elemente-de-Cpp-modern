df12dec5cdf3c79cb0d6c2389a28760e1bd0ffed
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-10-09 01:28:07 +0300

    Write game state to console in the update loop in QuartoGame.

--- Quarto/QuartoGame.cpp
+++ Quarto/QuartoGame.cpp
@@ -21,7 +21,17 @@ void QuartoGame::Run()
 	// update
 	while (true)
 	{
+        std::system("cls");  // on Windows
+
+        std::cout << "The board looks like this:" << std::endl;
+        std::cout << board << std::endl;
+        std::cout << "The unused pieces are the following:" << std::endl;
+        std::cout << unusedPieces << std::endl;
+
+        std::cout << firstPlayer << ", which piece do you want to pick for " << secondPlayer << '?' << std::endl;
 		Piece pickedPiece{ Player::PickPiece(std::cin, unusedPieces) };
+
+        std::cout << secondPlayer << ", where do you want to place the piece on the board?" << std::endl;
         Player::PlacePiece(std::cin, std::move(pickedPiece), board);
 	}
 }
