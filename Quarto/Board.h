9324039af258b419f773d6cdf5f9f03a55e3edc6
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-10-31 16:54:08 +0200

    Added declaration of views and IsFull to Board.

--- Quarto/Board.h
+++ Quarto/Board.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <array>
+#include <functional>
 #include <optional>
+#include <ranges>
 #include <tuple>
 
 #include "Piece.h"
@@ -9,6 +11,8 @@ class Board
 {
 public:
 	using Position = std::tuple<uint8_t, uint8_t>;
+	using BoardTransformer = std::function<const std::optional<Piece>& (size_t)>;
+	using LineView = std::ranges::transform_view<std::ranges::iota_view<size_t, size_t>, BoardTransformer>;
 
 public:
 	Board() = default;
@@ -18,9 +22,19 @@ public:
 	// Getter/Setter
 	std::optional<Piece>& operator [] (const Position& position);
 
+	// Views
+	LineView GetRowView(uint8_t row) const;
+	LineView GetColumnView(uint8_t column) const;
+	LineView GetMainDiagonalView() const;
+	LineView GetSecondaryDiagonalView() const;
+
+	bool IsFull() const;
+
 	friend std::ostream& operator << (std::ostream& os, const Board& board);
 
 private:
+	LineView GetLineView(BoardTransformer&& transformer) const;
+
 	static constexpr size_t kWidth = 4;
 	static constexpr size_t kHeight = 4;
 
