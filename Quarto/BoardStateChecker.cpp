cb7ecd1c77b925208afb00b131441bd860e0d50d
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-11-01 15:37:17 +0200

    Added dummy implementation for Check and added local HaveSharedCharacteristics function in BoardStateChecker.

--- Quarto/BoardStateChecker.cpp
+++ Quarto/BoardStateChecker.cpp
@@ -1,1 +1,14 @@
 #include "BoardStateChecker.h"
+
+namespace
+{
+	bool HaveSharedCharacteristics(const Board::LineView& view)
+	{
+		return false;
+	}
+}
+
+BoardStateChecker::State BoardStateChecker::Check(const Board& board, const Board::Position& position)
+{
+	return State{};
+}
