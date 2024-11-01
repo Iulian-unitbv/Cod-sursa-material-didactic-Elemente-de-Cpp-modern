40162d47183c825cdde98ce06906dbd3424417eb
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-11-01 15:31:04 +0200

    Added some shortcuts and overloaded ToString in BoardStateCheckerTests.

--- QuartoTests/BoardStateCheckerTests.cpp
+++ QuartoTests/BoardStateCheckerTests.cpp
@@ -4,6 +4,35 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+namespace
+{
+	// define shortcuts, to avoid huge line
+	using Body = Piece::Body;
+	using Color = Piece::Color;
+	using Height = Piece::Height;
+	using Shape = Piece::Shape;
+	using State = BoardStateChecker::State;
+}
+
+namespace Microsoft::VisualStudio::CppUnitTestFramework
+{
+	// specialize ToString method to allow comparing State types in Assert::AreEqual
+	template<>
+	std::wstring ToString<State>(const State& state)
+	{
+		switch (state)
+		{
+		case State::Win:
+			return ToString("Win");
+		case State::Draw:
+			return ToString("Draw");
+		case State::None:
+		default:
+			return ToString("None");
+		}
+	}
+}
+
 namespace QuartoTests
 {
 	TEST_CLASS(BoardStateCheckerTests)
