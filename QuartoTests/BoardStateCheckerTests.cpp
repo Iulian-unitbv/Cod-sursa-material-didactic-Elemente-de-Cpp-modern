7dddd36b4cfac1184e63766758af24068b4d35a5
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-11-01 14:54:19 +0200

    Added empty BoardStateCheckerTests and use BoardStateChecker.obj.

---
+++ QuartoTests/BoardStateCheckerTests.cpp
@@ -0,0 +1,13 @@
+#include "CppUnitTest.h"
+
+#include "BoardStateChecker.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace QuartoTests
+{
+	TEST_CLASS(BoardStateCheckerTests)
+	{
+	public:
+	};
+}
