8505f2638cf369e00527476c8af117b868e012f8
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-11-01 17:04:29 +0200

    Implemented Check in BoardStateChecker.

--- Quarto/BoardStateChecker.cpp
+++ Quarto/BoardStateChecker.cpp
@@ -10,5 +10,16 @@ namespace
 
 BoardStateChecker::State BoardStateChecker::Check(const Board& board, const Board::Position& position)
 {
-	return State{};
+	const auto& [row, column] = position;
+	const Board::LineView& rowView{ board.GetRowView(row) };
+
+	return
+		HaveSharedCharacteristics(rowView) ||
+		HaveSharedCharacteristics(board.GetColumnView(column)) ||
+		row == column && HaveSharedCharacteristics(board.GetMainDiagonalView()) ||
+		row == (rowView.size() - column - 1) && HaveSharedCharacteristics(board.GetSecondaryDiagonalView()) ?
+		BoardStateChecker::State::Win :
+		board.IsFull() ?
+		BoardStateChecker::State::Draw :
+		BoardStateChecker::State::None;
 }
