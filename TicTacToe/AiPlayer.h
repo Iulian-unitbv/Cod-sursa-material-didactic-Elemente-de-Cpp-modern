4e58012600747a46a36518b2e6fe815ce5212b3d
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-11-11 01:13:36 +0200

    Added constructor with board and GetPossibleActions method to AiPlayer header.

--- TicTacToe/AiPlayer.h
+++ TicTacToe/AiPlayer.h
@@ -1,10 +1,20 @@
 #pragma once
 #include "IPlayer.h"
 
+#include <vector>
+
 class AiPlayer : public IPlayer
 {
 public:
+	AiPlayer(const Board& board);
+
 	// Inherited via IPlayer
 	Board::Position GetNextAction() override;
 	const std::string& GetName() const override;
+
+private:
+	std::vector<Board::Position> GetPossibleActions() const;
+
+private:
+	const Board& m_board;
 };
