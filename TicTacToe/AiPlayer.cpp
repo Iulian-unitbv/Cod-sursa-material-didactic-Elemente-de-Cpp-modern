f429607818cdccf76ba3a572c2de2afda74ad7ef
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-11-11 00:04:28 +0200

    Overwrote pure virtual methods from IPlayer and added basic implementation to them.

--- TicTacToe/AiPlayer.cpp
+++ TicTacToe/AiPlayer.cpp
@@ -1,1 +1,12 @@
 #include "AiPlayer.h"
+
+Board::Position AiPlayer::GetNextAction()
+{
+	return { 2, 2 };
+}
+
+const std::string& AiPlayer::GetName() const
+{
+	static std::string name("AiPlayer");
+	return name;
+}
--- TicTacToe/AiPlayer.h
+++ TicTacToe/AiPlayer.h
@@ -3,4 +3,8 @@
 
 class AiPlayer : public IPlayer
 {
+public:
+	// Inherited via IPlayer
+	Board::Position GetNextAction() override;
+	const std::string& GetName() const override;
 };
