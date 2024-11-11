d26a8396d57ef1e155e2ab1ea6ff6fb5feee1821
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-11-11 02:03:13 +0200

    Implemented constructor and GetPossibleActions, and updated GetNextAction in AiPlayer.

--- TicTacToe/AiPlayer.cpp
+++ TicTacToe/AiPlayer.cpp
@@ -1,12 +1,36 @@
 #include "AiPlayer.h"
 
+AiPlayer::AiPlayer(const Board& board) : m_board(board)
+{
+	// empty
+}
+
 Board::Position AiPlayer::GetNextAction()
 {
-	return { 2, 2 };
+	std::vector<Board::Position> possibleActions = GetPossibleActions();
+	return possibleActions.front();
 }
 
 const std::string& AiPlayer::GetName() const
 {
 	static std::string name("AiPlayer");
 	return name;
+}
+
+std::vector<Board::Position> AiPlayer::GetPossibleActions() const
+{
+	std::vector<Board::Position> possibleActions;
+	for (uint8_t i = 0; i < Board::kRows; ++i)
+	{
+		for (uint8_t j = 0; j < Board::kColumns; ++j)
+		{
+			Board::Position position = { i, j };
+			if (!m_board[position])
+			{
+				possibleActions.emplace_back(position);
+			}
+		}
+	}
+
+	return possibleActions;
 }
