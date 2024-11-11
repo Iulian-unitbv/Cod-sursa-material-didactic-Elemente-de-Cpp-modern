7354cdf488961122b10371e2d045407bbdc74749
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-11-12 01:46:02 +0200

    Added vector for previous state action hashes, and FeedReward method to AiPlayer.

--- TicTacToe/AiPlayer.h
+++ TicTacToe/AiPlayer.h
@@ -13,10 +13,13 @@ public:
 	Board::Position GetNextAction() override;
 	const std::string& GetName() const override;
 
+	void FeedReward(float target);
+
 private:
 	std::vector<Board::Position> GetPossibleActions() const;
 
 private:
 	const Board& m_board;
 	std::unordered_map<int64_t, float> m_stateActionCosts;
+	std::vector<int64_t> m_previousStateActions;
 };
