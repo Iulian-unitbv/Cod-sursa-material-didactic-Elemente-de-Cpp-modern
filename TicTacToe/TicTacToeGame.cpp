44a84c3bbea77efdbb80f8b8daf836dacf280809
Arpad Kerestely <k_arpi2004@yahoo.co.uk>
2024-11-12 03:28:40 +0200

    Implemented training and testing of the AiPlayer.

--- TicTacToe/TicTacToeGame.cpp
+++ TicTacToe/TicTacToeGame.cpp
@@ -9,45 +9,66 @@
 void TicTacToeGame::Run()
 {
 	Board board;
-	std::unique_ptr<IPlayer> firstPlayer = std::make_unique<HumanPlayer>("Player 1");
+	std::unique_ptr<IPlayer> firstPlayer = std::make_unique<AiPlayer>(board);
 	std::unique_ptr<IPlayer> secondPlayer = std::make_unique<AiPlayer>(board);
 
-	ConsoleRenderer renderer;
+	ConsoleRenderer renderer(false);
 
-	IPlayer* currentPlayer = firstPlayer.get();
-	IPlayer* nextPlayer = secondPlayer.get();
-	Board::Piece currentPiece = Board::Piece::Cross;
-	Board::Piece nextPiece = Board::Piece::Nought;
-
-	bool isGameActive = true;
-	while (isGameActive)
+	const uint32_t numberOfPlays = 10000;
+	for (uint32_t currentPlay = 0; currentPlay < numberOfPlays; ++currentPlay)
 	{
-		renderer.Draw(board);
-		bool isInputOk = false;
-		while (!isInputOk)
+		// on last iteration, play againt the ai as human
+		if (currentPlay == numberOfPlays - 1)
+
 		{
-			const Board::Position& position = currentPlayer->GetNextAction();
-			if (!board[position])
-			{
-				board[position] = currentPiece;
-				isInputOk = true;
-			}
+			renderer.SetActive(true);
+			secondPlayer = std::make_unique<HumanPlayer>("HumanPlayer");
 		}
 
-		auto state = board.Check();
-		switch (state)
+		board.Reset();
+		IPlayer* currentPlayer = firstPlayer.get();
+		IPlayer* nextPlayer = secondPlayer.get();
+		Board::Piece currentPiece = Board::Piece::Cross;
+		Board::Piece nextPiece = Board::Piece::Nought;
+
+		bool isGameActive = true;
+		while (isGameActive)
 		{
-		case Board::State::Win:
-			renderer.ShowEndScreen(currentPlayer);
-			isGameActive = false;
-			break;
-		case Board::State::Draw:
-			renderer.ShowEndScreen();
-			isGameActive = false;
-			break;
-		case Board::State::None:
-			std::swap(currentPlayer, nextPlayer);
-			std::swap(currentPiece, nextPiece);
+			renderer.Draw(board);
+			bool isInputOk = false;
+			while (!isInputOk)
+			{
+				const Board::Position& position = currentPlayer->GetNextAction();
+				if (!board[position])
+				{
+					board[position] = currentPiece;
+					isInputOk = true;
+				}
+			}
+
+			auto state = board.Check();
+			switch (state)
+			{
+			case Board::State::Win:
+				renderer.ShowEndScreen(currentPlayer);
+				if (auto player1 = dynamic_cast<AiPlayer*>(currentPlayer))
+					player1->FeedReward(1.f);
+				if (auto player2 = dynamic_cast<AiPlayer*>(nextPlayer))
+					player2->FeedReward(0.f);
+				isGameActive = false;
+				break;
+			case Board::State::Draw:
+				renderer.ShowEndScreen();
+				if (auto player1 = dynamic_cast<AiPlayer*>(currentPlayer))
+					player1->FeedReward(0.5f);
+				if (auto player2 = dynamic_cast<AiPlayer*>(nextPlayer))
+					player2->FeedReward(0.5f);
+				isGameActive = false;
+				break;
+			case Board::State::None:
+				std::swap(currentPlayer, nextPlayer);
+				std::swap(currentPiece, nextPiece);
+			}
 		}
 	}
 }
